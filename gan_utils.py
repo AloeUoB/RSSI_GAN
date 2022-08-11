9e3b22579996239df5b29558b28745b6ba412e71
AloeUoB <110100893+AloeUoB@users.noreply.github.com>
2022-08-11 01:17:05 +0100

    Add files via upload

--- gan_utils.py
+++ gan_utils.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import pandas as pd
 import torch
 import torch.nn as nn
-from GAN.gan_model import Discriminator, Generator, initialize_weights
+from GAN.con_gan_rssi.model_wgan_gp import Discriminator, Generator, initialize_weights
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 from scipy import stats
@@ -15,7 +15,17 @@ from play_space import feature
 map_A = {'a': 'hallway_entrance', 'b': 'living_area_1',   'c': 'living_area_2',
           'd': 'living_area_3',     'e': 'living_area_4',       'f': 'bathroom',
           'g': 'bedroom_1',      'h': 'bedroom_2', 1: 'hallway_entrance',  2: 'bedroom',
-            3: 'living_area',    4: 'bathroom'}
+            3: 'living_area',    4: 'bathroom', 'hallway_entrance':1, 'bedroom':2,
+            'living_area':3,  'bathroom':4}
+
+map_B = {'a': 'hallway_lower', 'b': 'living_room',   'c': 'dining_room_1',
+          'd': 'dining_room_2',     'e': 'kitchen_1',       'f': 'kitchen_2',
+          'g': 'bathroom',      'h': 'hallway_upper',     'i': 'bedroom-two',
+          'j': 'bedroom-one', 'k': 'toilet',
+         1: 'hallway_lower', 2: 'kitchen', 3: 'living_room', 4: 'dining_room', 5: 'stairs_lower',
+        6: 'stairs_upper', 7: 'bathroom', 8: 'hallway_upper', 9: 'bedroom_2', 10:'bedroom_1', 11:'toilet',
+         'hallway_lower': 1, 'kitchen': 2, 'living_room': 3, 'dining_room': 4, 'stairs_lower': 5,
+        'stairs_upper': 6, 'bathroom': 7, 'hallway_upper': 8, 'bedroom_2': 9, 'bedroom_1':10}
 
 map_C = {'a': 'living_room_1', 'b': 'living_room_2',   'c': 'kitchen_1',
           'd': 'kitchen_2',     'e': 'kitchen_3',       'f': 'hallway_upper',
@@ -29,15 +39,6 @@ map_C = {'a': 'living_room_1', 'b': 'living_room_2',   'c': 'kitchen_1',
 
 map_D = {'a': 'living_room_B1', 'b': 'living_room_B2',   'c': 'living_room_A1',
           'd': 'living_room_A2',     'e': 'hallway_lower',       'f': 'kitchen',
-          'g': 'bathroom',      'h': 'hallway_upper',     'i': 'bedroom-two',
-          'j': 'bedroom-one_1', 'k': 'bedroom-one_2',
-         1: 'hallway_lower', 2: 'living_area_A', 3: 'living_area_B', 4: 'kitchen',
-         5: 'outside', 6: 'stairs', 7: 'bathroom', 8: 'hallway_upper', 9: 'bedroom_2', 10:'bedroom_1',
-         'hallway_lower': 1, 'living_area_A': 2, 'living_area_B': 3, 'kitchen': 4,
-         'outside': 5, 'stairs': 6, 'bathroom': 7, 'hallway_upper': 8, 'bedroom_2': 9, 'bedroom_1':10}
-
-map_B = {'a': 'living_room_B1*', 'b': 'living_room_B2*',   'c': 'living_room_A1*',
-          'd': 'living_room_A2*',     'e': 'hallway_lower*',       'f': 'kitchen*',
           'g': 'bathroom',      'h': 'hallway_upper',     'i': 'bedroom-two',
           'j': 'bedroom-one_1', 'k': 'bedroom-one_2',
          1: 'hallway_lower', 2: 'living_area_A', 3: 'living_area_B', 4: 'kitchen',
@@ -88,7 +89,7 @@ def MinMaxScaler(data):
 def renormalization(norm_data, min_val, max_val):
     return (norm_data * max_val) + min_val
 
-def windowing(ori_data, y, seq_len = 20, hop_size = 10):
+def windowing(ori_data, y, seq_len = 20, hop_size = 10, shuffle=True):
     windowed_data = []
     windowed_label = []
     # Cut data by sequence length
@@ -97,16 +98,19 @@ def windowing(ori_data, y, seq_len = 20, hop_size = 10):
         _y = stats.mode(y[i:i + seq_len])[0][0]
         windowed_data.append(_x)
         windowed_label.append(_y)
-
-    idx = np.random.permutation(len(windowed_data))
-    data = []
-    label = []
-    for i in range(len(windowed_data)):
-        data.append(windowed_data[idx[i]])
-        label.append(windowed_label[idx[i]])
-
-    data= np.asarray(data)
+    if shuffle:
+        idx = np.random.permutation(len(windowed_data))
+        data = []
+        label = []
+        for i in range(len(windowed_data)):
+            data.append(windowed_data[idx[i]])
+            label.append(windowed_label[idx[i]])
+    else:
+        data = windowed_data
+        label = windowed_label
+    data = np.asarray(data)
     label = np.asarray(label)
+
     return data, label
 
 def plot_all_rssi(rssi_plot, label_plot='house_C', ap_map=None):
@@ -130,16 +134,22 @@ def plot_all_rssi(rssi_plot, label_plot='house_C', ap_map=None):
     plt.show()
     plt.close()
 
-def plot_line_rssi_gan(rssi_plot, label_plot, transpose=False, house = None, house_map=None,
-                       full_scale=False, ymin=None, ymax=None, save=False, save_dir=None,model_name=None):
+def plot_line_rssi_gan(rssi_plot, label_plot, transpose=False, house = None, house_map=None, reduce=False,
+                       full_scale=False, ymin=None, ymax=None, save=False, save_dir=None, model_name=None, plot_idx=None):
     if transpose:
         rssi_plot =  np.transpose(rssi_plot)
 
     # house_map = {'A': map_A, 'B': map_A, 'C': map_A, 'D': map_A}
-    house_aps_label = {'A': ['f', 'd', 'c', 'b', 'a', 'h', 'e', 'g'],
-                       'B': ['g', 'h', 'd', 'j', 'i', 'b', 'c', 'e', 'k', 'a', 'f'],
-                       'C': ['g', 'd', 'f', 'k', 'c', 'b', 'a', 'j', 'h', 'i', 'e'],
-                       'D': ['g', 'e', 'j', 'k', 'b', 'a', 'i', 'f', 'd', 'h', 'c']}
+    if reduce:
+        house_aps_label = {'A': ['f', 'd', 'c', 'b', 'a', 'h', 'e', 'g'],
+                           'B': ['g', 'h', 'd', 'j', 'i', 'b', 'c', 'e', 'k', 'a', 'f'],
+                           'C': ['g', 'd', 'k', 'a', 'h', 'i'],
+                           'D': ['g', 'e', 'j', 'k', 'b', 'a', 'i', 'f', 'd', 'h', 'c']}
+    else:
+        house_aps_label = {'A': ['f', 'd', 'c', 'b', 'a', 'h', 'e', 'g'],
+                           'B': ['g', 'h', 'd', 'j', 'i', 'b', 'c', 'e', 'k', 'a', 'f'],
+                           'C': ['g', 'd', 'f', 'k', 'c', 'b', 'a', 'j', 'h', 'i', 'e'],
+                           'D': ['g', 'e', 'j', 'k', 'b', 'a', 'i', 'f', 'd', 'h', 'c']}
 
     rssi_plot = pd.DataFrame((rssi_plot), columns=house_aps_label[house])
 
@@ -163,7 +173,51 @@ def plot_line_rssi_gan(rssi_plot, label_plot, transpose=False, house = None, hou
     # plt.yticks(fontsize=20)
     plt.tight_layout()
     if save:
-        plt.savefig(save_dir + model_name + '_' + label_plot +'.png')
+        plt.savefig(save_dir + model_name + label_plot +'_idx'+str(plot_idx)+'.png')
+    else:
+        plt.show()
+    plt.close()
+
+def plot_line_rssi_all(rssi_plot, label_plot, transpose=False, house = None, house_map=None, reduce=False,
+                       full_scale=False, ymin=None, ymax=None, save=False, save_dir=None, model_name=None):
+    if transpose:
+        rssi_plot =  np.transpose(rssi_plot)
+
+    # house_map = {'A': map_A, 'B': map_A, 'C': map_A, 'D': map_A}
+    if reduce:
+        house_aps_label = {'A': ['f', 'd', 'c', 'b', 'a', 'h', 'e', 'g'],
+                           'B': ['g', 'h', 'd', 'j', 'i', 'b', 'c', 'e', 'k', 'a', 'f'],
+                           'C': ['g', 'd', 'k', 'a', 'h', 'i'],
+                           'D': ['g', 'e', 'j', 'k', 'b', 'a', 'i', 'f', 'd', 'h', 'c']}
+    else:
+        house_aps_label = {'A': ['f', 'd', 'c', 'b', 'a', 'h', 'e', 'g'],
+                           'B': ['g', 'h', 'd', 'j', 'i', 'b', 'c', 'e', 'k', 'a', 'f'],
+                           'C': ['g', 'd', 'f', 'k', 'c', 'b', 'a', 'j', 'h', 'i', 'e'],
+                           'D': ['g', 'e', 'j', 'k', 'b', 'a', 'i', 'f', 'd', 'h', 'c']}
+
+    rssi_plot = pd.DataFrame((rssi_plot), columns=house_aps_label[house])
+
+    if house_map:
+        rssi_plot = rssi_plot.rename(house_map, axis='columns')
+        label_plot = house_map[label_plot]
+
+    #plot the data
+    sns.set_theme(rc={'figure.figsize': (8, 4)})
+    sns.lineplot(data=rssi_plot, legend=True)
+    # sns.displot(rssi_plot, kind='kde', fill=fill, height=5, aspect=2.5)
+    # plt.xlim(-0.1, 21)
+    plt.ylim(ymin, ymax) #(-4, 3.5)
+    if full_scale:
+        plt.ylim(-130, 5)
+    plt.title("label %s" % (label_plot), fontsize=14)
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
+    plt.xlabel("Timepoint", fontsize=18)
+    # plt.xticks(fontsize=22)
+    plt.ylabel("RSSI(dB)", fontsize=18)
+    # plt.yticks(fontsize=20)
+    plt.tight_layout()
+    if save:
+        plt.savefig('GAN/lineplot_all/House_'+house+'/'+label_plot+'/' + model_name +'.png')
     else:
         plt.show()
     plt.close()
@@ -206,11 +260,20 @@ def get_mean_stat(windowed_data, windowed_label, number_APs, room_num, save=Fals
             ap_line = torch.full((1, 20), torch.mean(ap))
             ap_plot = torch.cat((ap_plot, ap_line), 0)
         if save:
-            plot_line_rssi_gan(ap_plot.cpu().detach().numpy(), room_label + 1, transpose=True, label_map=label_map,
-                               ap_map=ap_map, ymin=-0.1, ymax=1.1, save=True, model_name=str(plot_name))
+            plot_line_rssi_gan(ap_plot.cpu().detach().numpy(), room_label + 1, transpose=True, house=None, house_map=None,
+                               ymin=-0.1, ymax=1.1, save=True, model_name=str(plot_name))
         else:
-            plot_line_rssi_gan(ap_plot.cpu().detach().numpy(), room_label + 1, transpose=True, label_map=label_map,
-                               ap_map=ap_map, ymin=-0.1, ymax=1.1, save=False)
+            plot_line_rssi_gan(ap_plot.cpu().detach().numpy(), room_label + 1, transpose=True, house=None, house_map=None,
+                               ymin=-0.1, ymax=1.1, save=False)
+
+def get_stats(ori_data, label, norm = True ): # house_file = 'csv_house_' + house_name + '_fl.csv'
+    if norm:
+        norm_data, min_val, max_val = MinMaxScaler(ori_data)
+    else:
+        norm_data = ori_data
+    windowed_data, windowed_label = windowing(norm_data, label, seq_len=20, hop_size=10)
+    windowed_data = np.transpose(windowed_data, (0, 2, 1))
+    get_mean_stat(windowed_data, windowed_label, number_APs=11, room_num=9, save=False, plot_name=None)
 
 def generate_fake_data(gen_model, Z_DIM, num_fake, total_room, aps, device):
     labels_house = torch.from_numpy(np.full((num_fake[0]), 0)).to(device)
@@ -314,4 +377,26 @@ def model_test(X, y, model):
     print('F1 score:', f1 * 100)
 
     return predicted, true
+
+def get_fake_rssi(real_data_labels, num_epochs, total_number, NUM_CLASSES, APs, save_directory, model_name, device):
+    # generator parameter
+    Z_DIM = 11
+    CHANNELS_RSSI = 1
+    FEATURES_GEN = 32
+    WINDOW_SIZE = 20
+    GEN_EMBEDDING = 100
+    # load pretrained generator
+    gen = Generator(Z_DIM, CHANNELS_RSSI, FEATURES_GEN, NUM_CLASSES, APs, WINDOW_SIZE, GEN_EMBEDDING).to(device)
+    gen = load_GAN_model(gen, save_directory, model_name, num_epochs, device)
+    # get number of fake data
+    unique_y, counts_y = np.unique(real_data_labels, return_counts=True)
+    num_fake = []
+    for i in range (0, NUM_CLASSES):
+        if total_number-counts_y[i] > 0:
+            num_fake.append(total_number-counts_y[i])
+        else:
+            num_fake.append(0)
+    # generate fake data
+    fake_data, y_fake = generate_fake_data(gen, Z_DIM, num_fake=num_fake, total_room=NUM_CLASSES, aps= APs, device=device)
+    return fake_data, y_fake, gen
 
